15c1c73494175491cb0a4295d72753c68a7320ef
unknown <elton_pereira@live.com>
2021-04-04 20:51:17 -0300

    Vercel Cors, Fixed 2!

--- src/pages/api/V1/categories/list.js
+++ src/pages/api/V1/categories/list.js
@@ -1,6 +1,24 @@
-import micro from 'micro-cors'
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
 
-function MyApi(req, res) {
+const handler = (req, res) => {
   res.status(200).json({
     items: [
       {
@@ -22,6 +40,4 @@ function MyApi(req, res) {
   })
 }
 
-const cors = micro()
-
-export default cors(MyApi)
+module.exports = allowCors(handler)
